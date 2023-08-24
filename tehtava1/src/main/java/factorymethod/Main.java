273c1e974d62387859efd0064c98219335696bce
RauliYoung <tristan.ellenberg@gmail.com>
2023-08-24 12:20:07 +0300

    source for first assignment

--- tehtava1/src/main/java/factorymethod/Main.java
+++ tehtava1/src/main/java/factorymethod/Main.java
@@ -1,7 +1,9 @@
 package factorymethod;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AterioivaOtus opettaja = new Opettaja();
+        opettaja.aterioi();
     }
 }
