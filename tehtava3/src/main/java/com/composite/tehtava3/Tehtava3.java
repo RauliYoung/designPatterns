4d6888abf0227b8ae5a1565846e1e7fd33c33314
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-13 12:39:31 +0300

    composite project root

---
+++ tehtava3/src/main/java/com/composite/tehtava3/Tehtava3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.composite.tehtava3;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class Tehtava3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
