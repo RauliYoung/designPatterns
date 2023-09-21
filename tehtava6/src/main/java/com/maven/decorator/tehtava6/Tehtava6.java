0701672a23fd84e00c5842ab573e6c0d4aa4eb72
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-21 09:46:50 +0300

    runko decoratorille

---
+++ tehtava6/src/main/java/com/maven/decorator/tehtava6/Tehtava6.java
@@ -0,0 +1,12 @@
+package com.maven.decorator.tehtava6;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class Tehtava6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
