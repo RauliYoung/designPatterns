c35c1c6ed2a3149c941d42328633ababa7db8bf0
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-21 18:59:20 +0300

    pokemon class

---
+++ tehtava7/src/main/java/com/maven/state/tehtava7/Pokemon.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.maven.state.tehtava7;
+
+/**
+ *
+ * @author tristan
+ */
+public class Pokemon {
+    private int hp;
+    private int xp;
+
+    public Pokemon() {
+        this.hp = 20;
+        this.xp = 0;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+    
+    
+}
