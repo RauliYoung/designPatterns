86f47f9c0c788776257d09f86cf3b76c6821e39e
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-21 17:39:07 +0300

    runko state tehtavalle

---
+++ tehtava7/src/main/java/com/maven/state/tehtava7/Tehtava7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maven.state.tehtava7;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class Tehtava7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
