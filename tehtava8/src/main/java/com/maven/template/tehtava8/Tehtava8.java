1866b59fd31ba2a5d8338d9d1e7cb27238d91940
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-28 09:17:24 +0300

    template body

---
+++ tehtava8/src/main/java/com/maven/template/tehtava8/Tehtava8.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maven.template.tehtava8;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class Tehtava8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
