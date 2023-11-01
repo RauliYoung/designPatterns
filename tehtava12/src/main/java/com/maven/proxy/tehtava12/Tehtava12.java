442813aa5f84947e5372491b4af67819699470d4
RauliYoung <tristan.ellenberg@gmail.com>
2023-11-01 09:37:07 +0200

    proxy skelly

---
+++ tehtava12/src/main/java/com/maven/proxy/tehtava12/Tehtava12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maven.proxy.tehtava12;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class Tehtava12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
